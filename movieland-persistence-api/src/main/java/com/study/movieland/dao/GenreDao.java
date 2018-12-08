ec291170747e242466dfcc20efa7f9979ff92df7
Oleksandr Zhuravlov <alex.shadoof@gmail.com>
2018-12-08 14:19:22 +0200

    [b-10] add and edit movie  - replace thrown exception in default methods with  UnsupportedOperationException

--- movieland-persistence-api/src/main/java/com/study/movieland/dao/GenreDao.java
+++ movieland-persistence-api/src/main/java/com/study/movieland/dao/GenreDao.java
@@ -2,7 +2,6 @@ package com.study.movieland.dao;
 
 import com.study.movieland.entity.Genre;
 import com.study.movieland.entity.Movie;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
@@ -15,10 +14,10 @@ public interface GenreDao {
     List<Genre> getByMovieId(int movieId);
 
     default void addReference(Movie movie) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
-    default void editReference(Movie movie){
-        throw new NotImplementedException();
+    default void editReference(Movie movie) {
+        throw new UnsupportedOperationException();
     }
 }
