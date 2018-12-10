7840b0efba06d2c012b244290106029b340a20d7
Oleksandr Zhuravlov <alex.shadoof@gmail.com>
2018-12-10 18:33:01 +0200

    [b-10] add Transactional

--- movieland-service-default/src/main/java/com/study/movieland/service/impl/DefaultMovieService.java
+++ movieland-service-default/src/main/java/com/study/movieland/service/impl/DefaultMovieService.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -53,6 +54,7 @@ public class DefaultMovieService implements MovieService {
     }
 
     @Override
+    @Transactional
     public Movie getById(int id, MovieRequestParam movieRequestParam) {
         logger.info("get Movies by id {}", id);
         Movie movie = movieDao.getById(id);
